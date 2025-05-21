8b0415bcc759bc10a3f53c7dfde20c1abf31f3c8
choeunjeonghnu <145742397+choeunjeonghnu@users.noreply.github.com>
2025-05-21 17:36:57 +0900

    Update button.py

--- button.py
+++ button.py
@@ -1,6 +1,7 @@
 from evdev import InputDevice, ecodes
 import select
 import sys
+from motor import motor_control
 
 # 블루투스 버튼 디바이스 경로
 DEVICE_PATH = "/dev/input/event3"
@@ -30,8 +31,11 @@ while True:
         if event and event.type == ecodes.EV_KEY and event.code == ecodes.KEY_VOLUMEUP and event.value == 1:
             if not state:
                 state = True
+                motor_control(True)
                 #음성 듣는 함수 불러오기
 
             else:
                 state = False
+                motor_control(False)
                 #음성 듣기 중지 + 음성->텍스트 변환 함수 불러오기
+
